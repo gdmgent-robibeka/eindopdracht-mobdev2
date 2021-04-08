e07084cd48e5a98f730588ec7d84c4d031df8873
gdmgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2021-04-08 11:21:25 +0200

    Initial commit fixed

---
+++ api/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send(process.env.VARIABLE_ONE);
+});
+
+app.listen(port, () => {
+  console.log(`Server runnning at http://localhost:${port}`);
+});
---
+++ app/src/App/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="display-1">Hello world!</h1>
+    </div>
+  )
+};
+
+export default App;
