79bfd0bdda0e0aedd8356a649506cab1a8216137
gdmgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2021-04-08 16:30:12 +0200

    Add Prettier

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.send(process.env.VARIABLE_ONE);
--- app/src/App/App.js
+++ app/src/App/App.js
@@ -3,7 +3,7 @@ const App = () => {
     <div className="container">
       <h1 className="display-1">Hello world!</h1>
     </div>
-  )
+  );
 };
 
 export default App;
